fa5ec5efd7127e187d7795d10bc6b5237b047b94
Manuel Alonso <alonsomanuel08.06@gmail.com>
2025-12-23 14:54:16 -0300

    Correccion del manejo de rutas y se añadieron los endpoints para filtrar peliculas y series por su id

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import movies_list, series_list
+from .views import movies_list, series_list, movie_detail, serie_detail
 
 urlpatterns = [
     path("movies/", movies_list),
     path("series/", series_list),
+    path("movies/<int:id>/", movie_detail),
+    path("series/<int:id>/", serie_detail),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 from .models import Movies, Series
 from .serializers import MovieSerializer, SeriesSerializer
@@ -8,12 +9,25 @@ from .serializers import MovieSerializer, SeriesSerializer
 
 @api_view(['GET'])
 def movies_list(request):
-    movies = Movies.objects.all()
+    movies = get_list_or_404(Movies)
     serializer = MovieSerializer(movies, many=True)
-    return Response({"Peliculas": serializer.data})
+    return Response({"Peliculas": serializer.data}, 200)
+
+@api_view(['GET'])    
+def series_list(request):
+    series = get_list_or_404(Series)
+    serializer = SeriesSerializer(series, many=True)
+    return Response({"Series": serializer.data}, 200)
+    
+@api_view(['GET'])
+def movie_detail(request, id):
+    movie = get_object_or_404(Movies, pk=id)
+    serializer = MovieSerializer(movie)
+    return Response({"Pelicula": serializer.data}, 200)
 
 @api_view(['GET'])
-def series_list(request):
-    series = Series.objects.all()
-    serializer = SeriesSerializer(series, many=True)
-    return Response({"Series": serializer.data})
+def serie_detail(request, id):
+    serie = get_object_or_404(Series, pk=id)
+    serializer = SeriesSerializer(serie)
+    return Response({"Serie": serializer.data}, 200)
+    
